17ee28ce92416562ca3374f0aaacef1b4438c9e9
Nadia Potteiger <nyadiia@pm.me>
2023-07-12 16:10:02 -0500

    added intel file and farewell configuration

--- flake.nix
+++ flake.nix
@@ -86,6 +86,19 @@
           }
         ];
       };
+      farewell = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [
+          ./nixos/farewell/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.nyadiia = import ./home-manager/farewell/home.nix;
+          }
+        ];
+      };
     };
   };
 }
---
+++ nixos/farewell/configuration.nix
@@ -0,0 +1,118 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ inputs, config, pkgs, ... }:
+
+let inherit (inputs) ssh-keys;
+in {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../common.nix
+  ];
+
+  nixpkgs.hostPlatform.system = "x86_64-linux";
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.networkmanager.enable = true;
+  networking.hostName = "farewell";
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
+
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  # services.xserver.xkbOptions = {
+  #   "eurosign:e";
+  #   "caps:escape" # map caps to escape.
+  # };
+
+  # sound.enable = true;
+  hardware.pulseaudio.enable = false;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # User info
+  programs.fish.enable = true;
+
+  users.users.nyadiia = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "libvirtd" "docker" ];
+    home = "/home/nyadiia";
+    shell = pkgs.fish;
+    # !! please use home-manager if you can !!
+    packages = with pkgs; [ any-nix-shell ];
+    openssh.authorizedKeys.keyFiles = [ ssh-keys.outPath ];
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    docker
+    docker-compose
+  ];
+
+  # environment.gnome.excludePackages = (with pkgs; [ 
+  # gnome-photos gnome-tour
+  #   ]) ++ (with pkgs.gnome; [
+  # gnome-music
+  # gedit
+  # epiphany
+  # geary
+  # evince
+  # gnome-characters
+  # totem
+  # tali
+  # iagno
+  # hitori
+  # atomix
+  #   ]);
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
